56fddda75f2d9aad7a49ccd7645c02d3f9fa6eed
Naren <narendas10@gmail.com>
2022-09-26 10:59:51 +0530

    timestamp

--- scripts/CNN_Classifier/components/prepare_callback.py
+++ scripts/CNN_Classifier/components/prepare_callback.py
@@ -9,7 +9,7 @@ class PrepareCallback:
 
     @property
     def _create_tb_callbacks(self):
-        timestamp = time.strftime("%Y-%m-%d-%H-%M-%S")
+        timestamp = time.strftime("[%Y-%m-%d]--[%H-%M-%S]")
         tb_running_log_dir = os.path.join(
             self.config.tensorboard_root_log_dir,
             f"tb_logs_at_{timestamp}",
